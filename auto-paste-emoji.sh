12fdf25a60bfb1d43fb162f954f2f7f82e6dd518
Corentin Girard <corentingirard.dev@gmail.com>
2021-06-23 17:49:54 +0200

    Initial commit

---
+++ auto-paste-emoji.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+log() {
+  printf "[$$] $1" | systemd-cat
+  echo "$1"
+}
+
+set +e
+
+while clipnotify || true;
+do
+  log "** Selection changed"
+  
+  emoji_picker=$(xdotool search -class plasma.emojier || echo 0)
+  clipboard=$(xclip -o -sel clip || echo "<empty clipboard>")
+
+  if [ $emoji_picker -gt 0 ] && echo $clipboard | is-emoji ; then
+    log "* Emoji picker found ($emoji_picker) : closing..."
+    xdotool key --window $emoji_picker Escape Escape
+
+    sleep 0.2
+
+    log "> Paste emoji: $clipboard"
+    xdotool key ctrl+v
+  fi
+done
